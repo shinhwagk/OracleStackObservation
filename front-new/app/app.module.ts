b90858b7b48252446e777d29238d6d1172cc575e
gk <shanghai_sl@hotmail.com>
2016-11-08 17:22:49 +0800

    modify date format

--- front-new/app/app.module.ts
+++ front-new/app/app.module.ts
@@ -1,14 +1,14 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {HttpModule} from '@angular/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import './rxjs-extensions';
 
-import {AppComponent} from './app.component'
-import {NodesComponent} from "./nodes.component";
-import {ReportDatabaseComponent} from "./report.database.component";
-import {ReportOsComponent} from "./report.os.component";
+import { AppComponent } from './app.component'
+import { NodesComponent } from "./nodes.component";
+import { ReportDatabaseComponent } from "./report.database.component";
+import { ReportOsComponent } from "./report.os.component";
 
-import {AppRoutingModule}     from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
