0d3b1ed897432118433c68878dda366211a79af8
gk <shanghai_sl@hotmail.com>
2016-11-11 15:37:09 +0800

    modify temp report

--- back/conf/reports/oracle/ts_temp_usage.sql
+++ back/conf/reports/oracle/ts_temp_usage.sql
@@ -16,50 +16,50 @@ SELECT d.tablespace_name "Name",
  WHERE d.tablespace_name = a.tablespace_name(+)
    AND d.tablespace_name = t.tablespace_name(+)
    AND d.extent_management like 'LOCAL'
-   AND d.contents like 'TEMPORARY';
+   AND d.contents like 'TEMPORARY'
    
-SELECT "SID",
-       "SERIAL#",
-       "STATUS",
-       "ACTION",
-       "MACHINE",
-       "MODULE",
-       "OSUSER",
-       "TERMINAL",
-       "PROGRAM",
-       SQL_ID,
-       (SELECT NB.SQL_TEXT FROM V$SQLAREA NB WHERE NB.SQL_ID = TT.SQL_ID) SQL_TEXT,
-       "sums(M)",
-       "sizes(G)",
-       "TEMP表空间占用情况"
-FROM   (SELECT v.SID,
-               v.SERIAL#,
-               v.STATUS,
-               v.ACTION,
-               v.MACHINE,
-               v.MODULE,
-               v.OSUSER,
-               v.TERMINAL,
-               v.PROGRAM,
-               v.SQL_ID,
-               SUM(blocks) * 8 / (1024) "sums(M)",
-               (SELECT round(SUM(bytes) / (1024 * 1024 * 1024), 3)
-                FROM   v$tempfile) "sizes(G)",
-               round((SUM(blocks) * 8 / (1024)) /
-                     (SELECT SUM(bytes) / (1024 * 1024) FROM v$tempfile),
-                     3) temp表空间占用情况
-        FROM   v$tempseg_usage t,
-               v$session       v
-        WHERE  t.SESSION_ADDR = v.SADDR
-        GROUP  BY v.SID,
-                  v.SERIAL#,
-                  v.STATUS,
-                  v.ACTION,
-                  v.MACHINE,
-                  v.MODULE,
-                  v.OSUSER,
-                  v.TERMINAL,
-                  v.PROGRAM,
-                  v.SQL_ID
-        ORDER  BY "sums(M)" DESC) TT
-WHERE  "sums(M)" >= 10
+-- SELECT "SID",
+--        "SERIAL#",
+--        "STATUS",
+--        "ACTION",
+--        "MACHINE",
+--        "MODULE",
+--        "OSUSER",
+--        "TERMINAL",
+--        "PROGRAM",
+--        SQL_ID,
+--        (SELECT NB.SQL_TEXT FROM V$SQLAREA NB WHERE NB.SQL_ID = TT.SQL_ID) SQL_TEXT,
+--        "sums(M)",
+--        "sizes(G)",
+--        "TEMP表空间占用情况"
+-- FROM   (SELECT v.SID,
+--                v.SERIAL#,
+--                v.STATUS,
+--                v.ACTION,
+--                v.MACHINE,
+--                v.MODULE,
+--                v.OSUSER,
+--                v.TERMINAL,
+--                v.PROGRAM,
+--                v.SQL_ID,
+--                SUM(blocks) * 8 / (1024) "sums(M)",
+--                (SELECT round(SUM(bytes) / (1024 * 1024 * 1024), 3)
+--                 FROM   v$tempfile) "sizes(G)",
+--                round((SUM(blocks) * 8 / (1024)) /
+--                      (SELECT SUM(bytes) / (1024 * 1024) FROM v$tempfile),
+--                      3) temp表空间占用情况
+--         FROM   v$tempseg_usage t,
+--                v$session       v
+--         WHERE  t.SESSION_ADDR = v.SADDR
+--         GROUP  BY v.SID,
+--                   v.SERIAL#,
+--                   v.STATUS,
+--                   v.ACTION,
+--                   v.MACHINE,
+--                   v.MODULE,
+--                   v.OSUSER,
+--                   v.TERMINAL,
+--                   v.PROGRAM,
+--                   v.SQL_ID
+--         ORDER  BY "sums(M)" DESC) TT
+-- WHERE  "sums(M)" >= 10
