8bcf3390d336925f84c700d906e57a0fde2c43c8
gk <shanghai_sl@hotmail.com>
2016-11-14 17:56:31 +0800

    save

--- back/conf/reports/oracle/num_index_5.sql
+++ back/conf/reports/oracle/num_index_5.sql
@@ -1,9 +1,9 @@
 select owner,table_name, count(*) cnt
-  from dba_indexes 
- where owner not in ('XDB', 
+  from dba_indexes
+ where owner not in ('XDB',
                      'APEX_030200', 
                      'DBSNMP','ORDDATA')
    AND owner not like '%SYS%'
  group by owner,table_name
-having count(*) >= 5
+having count(*) >= 6
 order by cnt desc
