f1865ef84791335912c94f71d70d91dbc900dbf9
gk <shanghai_sl@hotmail.com>
2016-11-30 14:02:12 +0800

    modify reports os backup_datapump.sh

--- back/conf/reports/os/backup_datapump.sh
+++ back/conf/reports/os/backup_datapump.sh
@@ -17,32 +17,47 @@ function fileOfDirCnt(){
   echo `$LS $1 | wc -l`
 }
 
-if [ -d $dmpDir ] && [ `fileOfDirCnt ${dmpDir}` -gt 1 ]; then
-      for logFileName in `${LS} ${dmpDir} | grep -e 'log$'`
-      do
-        logFile="$dmpDir/$logFileName"
-        dmpFileName=`echo ${logFileName} | sed 's/log$/dmp/'`
-        dmpFile="$dmpDir/$dmpFileName"
-        cat ${logFile} | tail -n 1 | grep 'successfully' >/dev/null 2>&1
-        if [ $? == 0 ]; then
-          if [ -f $dmpFile ]; then
-            rs=${rs}"[\"${dmpFileName}\",\"successfully\"]"
-          else
-            rs=${rs}"[\"${dmpFileName}\",\"File no exist\"]"
-          fi
-        else
-          rs=${rs}"[\"${dmpFileName}\",\"dump failure\"]"
-        fi
-      done
-      rs=`echo $rs | sed 's/\]\[/\],\[/g'`
-      rs="["`setColumnName ${dmpDir}`${rs}"]"
-elif [ -d $nfsDir ] && [ `fileOfDirCnt ${nfsDir}` -gt 1 ]; then
-     rs='["none","none"]'
-     rs="["`setColumnName $nfsDir`${rs}"]"
+if [ -d $nfsDir ] && [ `fileOfDirCnt ${nfsDir}` -gt 1 ]; then
+  for logFileName in `${LS} ${nfsDir} | grep -e 'log$'`
+  do
+    logFile="$nfsDir/$logFileName"
+    dmpFileName=`echo ${logFileName} | sed 's/log$/dmp/'`
+    dmpFile="$nfsDir/$dmpFileName"
+    cat ${logFile} | tail -n 1 | grep 'successfully' >/dev/null 2>&1
+    if [ $? == 0 ]; then
+      if [ -f $dmpFile ]; then
+        rs=${rs}"[\"${dmpFileName}\",\"successfully\"]"
+      else
+        rs=${rs}"[\"${dmpFileName}\",\"File no exist\"]"
+      fi
+    else
+      rs=${rs}"[\"${dmpFileName}\",\"dump failure\"]"
+    fi
+  done
+  rs=`echo $rs | sed 's/\]\[/\],\[/g'`
+  rs="["`setColumnName ${nfsDir}`${rs}"]"
+elif [ -d $dmpDir ] && [ `fileOfDirCnt ${dmpDir}` -gt 1 ]; then
+  for logFileName in `${LS} ${dmpDir} | grep -e 'log$'`
+  do
+    logFile="$dmpDir/$logFileName"
+    dmpFileName=`echo ${logFileName} | sed 's/log$/dmp/'`
+    dmpFile="$dmpDir/$dmpFileName"
+    cat ${logFile} | tail -n 1 | grep 'successfully' >/dev/null 2>&1
+    if [ $? == 0 ]; then
+      if [ -f $dmpFile ]; then
+        rs=${rs}"[\"${dmpFileName}\",\"successfully\"]"
+      else
+        rs=${rs}"[\"${dmpFileName}\",\"File no exist\"]"
+      fi
+    else
+      rs=${rs}"[\"${dmpFileName}\",\"dump failure\"]"
+    fi
+  done
+  rs=`echo $rs | sed 's/\]\[/\],\[/g'`
+  rs="["`setColumnName ${dmpDir}`${rs}"]"
 else
-     rs='["none","none"]'
-     rs="["`setColumnName ${nfsDir}"_&&_"${dmpDir}`${rs}"]"
+  rs='["none","none"]'
+  rs="["`setColumnName ${nfsDir}"_&&_"${dmpDir}`${rs}"]"
 fi
 
 echo $rs
-
