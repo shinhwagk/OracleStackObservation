11bdcd543cc2f2b22f9917b318e79187055ecf40
gk <shanghai_sl@hotmail.com>
2016-12-08 18:15:56 +0800

    save

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+var path = require('path');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/main.ts',
+  output: {
+    path: './dist',
+    filename: 'bundle.min.js'
+  },
+  module: {
+    loaders: [{ test: /\.ts$/, loader: 'ts-loader' }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.ts']
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: 'app/**/*.html', to: '.' },
+      { from: 'app/**/*.css', to: '.' },
+      { from: 'styles.css', to: '.' },
+      { from: 'systemjs.config.js', to: '.' }
+    ]),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      title: 'Note Back',
+      inject: 'head'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      output: { comments: false }
+    })]
+};
