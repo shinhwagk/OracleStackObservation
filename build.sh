9ba38acb13afc58ac87ba83d2e2dd8da75d4b761
shinhwagk <shanghai_sl@hotmail.com>
2016-12-09 15:30:53 +0800

    update docker-compose.yml build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 mkdir -p /opt/OracleStackObservation && cd /opt/OracleStackObservation;
 curl -LsSf https://raw.githubusercontent.com/shinhwagk/OracleStackObservation/build/docker-compose.yml > docker-compose.yml
+
 git clone -b front https://github.com/shinhwagk/OracleStackObservation front --depth=1
+git clone -b nginx https://github.com/shinhwagk/OracleStackObservation nginx --depth=1
 
 docker-compose up
