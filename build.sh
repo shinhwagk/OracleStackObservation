0c4a74028e4ba09ba2cfe4cd6b8bf075624db111
gk <shanghai_sl@hotmail.com>
2016-12-09 13:25:35 +0800

    save

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 mkdir -p /opt/OracleStackObservation && cd /opt/OracleStackObservation;
 curl -LsSf https://raw.githubusercontent.com/shinhwagk/OracleStackObservation/build/docker-compose.yml > docker-compose.yml
 git clone -b front https://github.com/shinhwagk/OracleStackObservation front --depth=1
+
+docker-compose up
