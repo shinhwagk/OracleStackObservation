c9176076264a02f790e8606fec112ef845bfddf4
gk <shanghai_sl@hotmail.com>
2016-12-12 14:01:18 +0800

    tmp save

--- deploy_of_local_powershell.ps1
+++ deploy_of_local_powershell.ps1
@@ -1,4 +1,6 @@
 ssh root@mydocker.com "mkdir -p /opt/OracleStackObservation"
+
 scp .\build.sh root@mydocker.com:/opt/OracleStackObservation/
 scp .\docker-compose.yml root@mydocker.com:/opt/OracleStackObservation/
+
 ssh root@mydocker.com "/bin/bash /tmp/oo/build.sh"
