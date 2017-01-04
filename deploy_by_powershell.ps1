111e34775364db6f9e93060e554b8e67ce06a8ba
gk <shanghai_sl@hotmail.com>
2017-01-04 15:18:55 +0800

    update

--- deploy_by_powershell.ps1
+++ deploy_by_powershell.ps1
@@ -1,6 +1,4 @@
-$rootDir            = "/opt" 
-$workName = "OracleStackObservation"
-$workDir            = $rootDir + "/" + $workName
+$workName           = "OracleStackObservation"
 $webServiceName     = "front"
 $queryServiceName   = "query"
 $sshServiceName     = "ssh"
@@ -8,14 +6,17 @@ $nginxServiceName   = "nginx"
 $configServiceName  = "config"
 $buildServiceName   = "build"
 
+$rootDir            = "/opt" 
+$workDir            = $rootDir + "/" + $workName
+
 function createService([string]$name) {
-  Set-Location $workDir
   if (Test-Path -Path $name -PathType Container) {
     Set-Location $name
     git pull
   } else {
     git clone --depth=1 -b $name https://github.com/shinhwagk/OracleStackObservation $name
   }
+  Set-Location $workDir
 }
 
 if (Test-Path -Path $workDir -PathType Container) {} else {
