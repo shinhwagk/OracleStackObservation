a5da9258babf6c2f03c99a4b646e562b1a2b4714
gk <shanghai_sl@hotmail.com>
2017-01-04 13:17:41 +0800

    save

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ app = Flask(__name__)
 
 @app.route("/v4/report/<ip>/<name>")
 def hello(ip,name):
-    command = 'ssh root@%s "curl -kL https://raw.githubusercontent.com/shinhwagk/OracleStackObservation/master/back/conf/reports/os/%s.sh | /bin/bash"' % (ip,name)
+    command = 'ssh oracle@%s "curl -kL https://raw.githubusercontent.com/shinhwagk/OracleStackObservation/master/back/conf/reports/os/%s.sh | /bin/bash"' % (ip,name)
     rs = os.popen(command)
     b = rs.readlines()[0]
     return Response(b,  mimetype='application/json')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
