a42184b5fb20f78f2a11926c1eee872464a1bb9e
shinhwagk <shanghai_sl@hotmail.com>
2017-01-06 22:33:24 +0800

    add ready.ps1

---
+++ ready.ps1
@@ -0,0 +1,6 @@
+git clone --depth=1 -b config https://github.com/shinhwagk/OracleStackObservation config
+git clone --depth=1 -b ssh https://github.com/shinhwagk/OracleStackObservation ssh
+git clone --depth=1 -b alert https://github.com/shinhwagk/OracleStackObservation alert
+git clone --depth=1 -b master https://github.com/shinhwagk/OracleStackObservation back
+git clone --depth=1 -b build https://github.com/shinhwagk/OracleStackObservation build
+git clone --depth=1 -b front https://github.com/shinhwagk/OracleStackObservation front
