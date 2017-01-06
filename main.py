90039d655c25d5a3d486ecf60e5e477776a95d9d
gk <shanghai_sl@hotmail.com>
2017-01-06 13:16:45 +0800

    save

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, Response, json
 app = Flask(__name__)
 
-@app.route("/v4/report/<ip>/<name>")
+@app.route("/v1/report/<ip>/<name>")
 def hello(ip,name):
     command = 'ssh oracle@%s "curl -kL https://raw.githubusercontent.com/shinhwagk/OracleStackObservation/master/back/conf/reports/os/%s.sh | /bin/bash"' % (ip,name)
     rs = os.popen(command)
